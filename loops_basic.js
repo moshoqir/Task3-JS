e45f9b5133a3fd1bf1635a793d5f0ef300e70439
moshoqir <you@example.com>
2024-12-29 10:44:07 +0300

    first commit

---
+++ loops_basic.js
@@ -0,0 +1,26 @@
+// 1- Write a program that prints numbers from 1 to 10 using a for loop.
+
+
+// 2-Write a program that prints the even numbers from 1 to 10 using a for loop.
+
+
+// 3- Write a program that prints the odd numbers from 1 to 10 using a while loop.
+
+
+// 4- Write a program that prints the multiplication table of a number entered by the user using a for loop.
+
+
+// 5- Write a program that calculates the sum of numbers from 1 to 100 using a while loop.
+
+
+// 6- Write a program that calculates the factorial of a number entered by the user using a for loop.
+
+
+// 7- Write a program that prints the Fibonacci series up to a certain number entered by the user using a while loop.
+
+
+// 8- Write a program that prints the reverse of the following numbers:
+//5 , 10 , 15 , 20
+// using a while loop.
+
+
---
+++ main.js
@@ -0,0 +1,98 @@
+//1
+
+// for (let i = 1; i <= 10; i++) {
+//   console.log(i);
+// }
+
+//2
+console.log("*********2**********");
+
+for (let i = 1; i <= 10; i++) {
+  if (i % 2 == 0) {
+    console.log(i);
+  }
+}
+
+
+
+// //3
+// console.log("*********3**********");
+
+// for (let i = 1; i <= 10; i++) {
+//   if (i % 2 !== 0) {
+//     console.log(i);
+//   }
+// }
+
+// //4
+// // console.log("*********4**********");
+
+// // var num = parseInt(prompt("Enter a number:"));
+
+// // for (i = 1; i <= 10; i++) {
+// //   var result = i * num;
+
+// //   console.log(`${num} * ${i} = ${result}`);
+// // }
+
+// console.log("*********5**********");
+// //5
+// var sum = 0;
+// var i = 1;
+
+// while (i <= 100) {
+//   sum += i;
+//   i++;
+// }
+
+// console.log(sum);
+
+// // for (i = 0; i <= 100; i++) {
+// //   sum += i;
+// // }
+
+// // console.log(sum);
+
+// console.log("*********6**********");
+
+// // var num2 = parseInt(prompt("Enter a number:"));
+
+// // var result2 = 1;
+
+// // for (i = num2; i > 0; i--) {
+// //   result2 *= i;
+// // }
+
+// // console.log(`The factorial of ${num2} = ${result2}`);
+
+// console.log("*********7**********");
+
+// // var limit = parseInt(prompt("Enter a number to limit the Fibonacci series:"));
+// // var a = 0,
+// //   b = 1; // Initialize the first two Fibonacci numbers
+// // var next = a; // Initialize the next number
+
+// // console.log("Fibonacci series up to " + limit + ":");
+
+// // while (next <= limit) {
+// //   // Loop until the next Fibonacci number exceeds the limit
+// //   console.log(next); // Print the current Fibonacci number
+// //   next = a + b; // Calculate the next number in the series
+// //   a = b; // Update 'a' to the previous 'b'
+// //   b = next; // Update 'b' to the newly calculated number
+// // }
+
+// console.log("*********8**********");
+
+// var rev = parseInt(prompt("Enter a number:"));
+
+// var num3 = rev;
+// while (num3 > 0) {
+//   console.log(num3);
+
+//   num3--;
+// }
+
+// // for (i = 5; i < 25; i += 5) {
+// //   console.log(i * -1);
+// // }
